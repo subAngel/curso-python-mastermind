678027d6075549c4039e3153ee4539f4561af308
Angel Zorrilla <whoangel.agl@gmail.com>
2022-02-23 00:52:58 -0600

    Funciones - ejercicio 4

---
+++ PrimerProyecto/EjerciciosPythonAlf/Funciones/Ejercicio4.py
@@ -0,0 +1,11 @@
+def facturar(cantidad, iva=21):
+    print('{:<10} {:<5}'.format('Cantidad $', 'IVA %'))
+    iva = iva/100
+    print('{:<10} {:<5}'.format(cantidad, iva))
+    print('---------------------')
+    print('Factura Total:', cantidad+cantidad*iva)
+
+
+facturar(90, 0)
+print('\n')
+facturar(100)
